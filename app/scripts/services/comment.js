a8bd360e38e9b093bb7c3305298d75ddad2296ee
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-05-18 16:47:19 +0200

    Implemented comment service

--- app/scripts/services/comment.js
+++ app/scripts/services/comment.js
@@ -8,3 +8,12 @@
  * Factory in the thesismarketApp.
  */
 
+angular.module('thesismarketApp')
+  .factory('Comment', ['$resource', 'ENV', function($resource, ENV) {
+    return $resource(ENV.api+'/comments/:id', null,
+      {
+        'query': { method:'GET', isArray: false },
+        'update': { method:'PUT' },
+        'remove': {method:'DELETE'}
+      });
+  }]);
