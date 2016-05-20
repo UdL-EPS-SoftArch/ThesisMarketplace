a61c8febeed76d6241a78d03903cbba32312cff3
Roberto García <roberto@rhizomik.net>
2016-05-20 18:20:49 +0200

    Catch and show list proposals errors

--- app/scripts/controllers/proposalsCtrl.js
+++ app/scripts/controllers/proposalsCtrl.js
@@ -11,12 +11,17 @@ angular.module('thesismarketApp')
   .controller('ProposalsCtrl', function($scope, Proposal) {
 
     $scope.proposals = [];
+    $scope.error = '';
 
-    Proposal.query().$promise.then(function (proposals) {
-      $scope.proposals = proposals._embeddedItems;
-      $scope.proposals.forEach(function(proposal) {
-        proposal.creator = proposal._resources("creator").get();
+    Proposal.query().$promise
+      .then(function (proposals) {
+        $scope.proposals = proposals._embeddedItems;
+        $scope.proposals.forEach(function(proposal) {
+          proposal.creator = proposal._resources('creator').get();
+        });
+      })
+      .catch(function (error) {
+        $scope.error = error;
       });
-    });
 
   });
