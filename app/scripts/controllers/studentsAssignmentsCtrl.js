f4626521d60bc46ee70b442f7e53ea08ddd2ffad
Raul <valls.raul@gmail.com>
2016-05-29 18:35:59 +0200

    Listing Working

--- app/scripts/controllers/studentsAssignmentsCtrl.js
+++ app/scripts/controllers/studentsAssignmentsCtrl.js
@@ -8,19 +8,22 @@
  * Controller of the thesismarketApp
  */
 
+alert("")
+
 angular.module('thesismarketApp')
   .controller('StudentsAssignmentsCtrl', function($scope, StudentAssignment) {
 
+    alert("Enter controller");
     $scope.studentsAssignments = [];
+    $scope.proposal;
+    alert("test1");
 
     StudentAssignment.query().$promise.then(function (studentsAssignments) {
       $scope.studentsAssignments = studentsAssignments._embeddedItems;
 
       $scope.studentsAssignments.forEach(function(studentAssignment) {
-          studentAssignment._resources('assigns').get().$promise.then(function(studentOffer) {
-            studentAssignment.assigns = studentOffer;
-            studentAssignment.assigns.agent = studentOffer._resources('agent').get();
-          });
+        studentAssignment.assigns = studentAssignment._resources("assigns").get()
+        $scope.proposal = studentAssignment.assigns.getTarget();
       });
 
     });
