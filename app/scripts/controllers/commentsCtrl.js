0608cc74424c05db033f35156146d86819d4b51d
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-06-21 02:00:54 +0200

    Show all comments list on init page

--- app/scripts/controllers/commentsCtrl.js
+++ app/scripts/controllers/commentsCtrl.js
@@ -10,29 +10,31 @@
 angular.module('thesismarketApp')
   .controller('CommentsCtrl', function ($scope, $location, $state, Comment) {
 
+    var API = "https://thesismarket-api.herokuapp.com";
     $scope.comments = [];
     $scope.error = '';
 
-    Comment.query().$promise
-      .then(function (comments) {
-        $scope.comments = comments._embeddedItems;
-        $scope.comments
-          .forEach(function (comment) {
-            comment.author = comment._resources('author').get();
-            comment.comments = comment._resources('comments').get();
-          });
-      })
-      .catch(function (error) {
-        $scope.error = error;
-      });
-
+    $scope.getComments = function () {
+      Comment.query().$promise
+        .then(function (comments) {
+          $scope.comments = comments._embeddedItems;
+          $scope.comments
+            .forEach(function (comment) {
+              comment.author = comment._resources('author').get();
+              comment.comments = comment._resources('comments').get();
+            });
+        })
+        .catch(function (error) {
+          $scope.error = error;
+        })
+    };
+    
     $scope.addComment = function () {
-      var API = "https://thesismarket-api.herokuapp.com";
       $scope.comment.comments = API + "/proposalPublications/" + $location.url().split('/')[2];
       Comment.save($scope.comment)
         .$promise
         .then(function (comment) {
-          $state.go("home");
+          $state.go('home');
         });
     };
   });
