40d78145bdbc90ed76044dc857c76c22bb438027
lobopetit <xavierbonell@gmail.com>
2016-06-22 15:53:06 +0200

    Edit proposal controller first implementation

--- app/scripts/controllers/editProposalCtrl.js
+++ app/scripts/controllers/editProposalCtrl.js
@@ -1,3 +1,29 @@
+
+'use strict';
+
 /**
- * Created by xavier on 23/05/16.
+ * @ngdoc function
+ * @name thesismarketApp.controller:EditProposalCtrl
+ * @description
+ * # EditProposalCtrl
+ * Controller of the thesismarketApp
  */
+angular.module('thesismarketApp')
+  .controller('EditProposalCtrl', function($scope, $stateParams, PublishProposals) {
+
+    $scope.publication = {};
+
+    $scope.error = '';
+
+    PublishProposals.query({id: $stateParams.id}).$promise
+      .then(function (publication) {
+        $scope.publication = publication;
+        $scope.publication.publishes = publication._resources('publishes').get();
+        $scope.publication.agent = publication._resources('agent').get();
+        $scope.publication.commentedBy = publication._resources('commentedBy').get();
+      })
+      .catch(function (error) {
+        $scope.error = error;
+      });
+
+  });
