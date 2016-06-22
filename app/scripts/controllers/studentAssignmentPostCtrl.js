2d17caf80e1543ac3ed21cf612e16c4757628461
Raul <valls.raul@gmail.com>
2016-06-22 12:49:45 +0200

    Populate select students with usernames, first try

--- app/scripts/controllers/studentAssignmentPostCtrl.js
+++ app/scripts/controllers/studentAssignmentPostCtrl.js
@@ -12,6 +12,7 @@ angular.module('thesismarketApp')
   .controller('StudentsAssignmentsCtrl', function($scope, StudentAssignment) {
 
     var selectProposals = document.getElementById("selProposal");
+    var selectUsers = document.getElementById("selStudents");
 
     $scope.proposals = [];
     $scope.error = '';
@@ -30,4 +31,20 @@ angular.module('thesismarketApp')
         $scope.error = error;
       });
 
+    $scope.students = [];
+    $scope.error = '';
+
+    User.query().$promise
+      .then(function (users) {
+        $scope.users = users._embeddedItems||[];
+        $scope.users.forEach(function(user) {
+          var opt = document.createElement("option");
+          opt.value= user.id;
+          opt.text = user.username;
+          selectUsers.appendChild(opt);
+        });
+      })
+      .catch(function (error) {
+        $scope.error = error;
+      });
   });
