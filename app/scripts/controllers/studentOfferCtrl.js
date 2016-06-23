c61eb2d339d6314bc3b0a42df1c8d98d73026385
Roberto García <roberto@rhizomik.net>
2016-06-23 13:48:48 +0200

    Fix quotes formatting warning

--- app/scripts/controllers/studentOfferCtrl.js
+++ app/scripts/controllers/studentOfferCtrl.js
@@ -9,7 +9,7 @@ angular.module('thesismarketApp')
     StudentOffer.query().$promise.then(function (studentOffers) {
       $scope.studentOffers = studentOffers._embeddedItems;
       $scope.studentOffers.forEach(function(studentOffer) {
-        studentOffer.agent = studentOffer._resources("agent").get();
+        studentOffer.agent = studentOffer._resources('agent').get();
       });
     });
 
